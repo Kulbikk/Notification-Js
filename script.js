04c663aa5e88542f69a13c1186e119510e488195
Patryk Kulbinski <100892074+Kulbikk@users.noreply.github.com>
2023-01-10 22:26:32 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -10,13 +10,15 @@ let amount = 3;
 noti.textContent = amount
 
 
+
 const takeAmount = () => {
     if (amount > 0 ) {
         amount--
         noti.textContent = amount
     } else {
          return
-}
+    }
+    
 }
 
 
@@ -25,20 +27,25 @@ clearNoti.onclick = () => {
     bgRemove.classList.remove('bg-Lightgrayishblue1');
     sndBgRemove.classList.remove('bg-Lightgrayishblue1');
     thirdBgRemove.classList.remove('bg-Lightgrayishblue1');
+    
 }
+
 
 
 bgRemove.onclick = () => {
-    takeAmount()
-    bgRemove.classList.remove('bg-Lightgrayishblue1')
+        takeAmount()
+        bgRemove.classList.remove('bg-Lightgrayishblue1')
+        bgRemove.onclick = null
 }
 
 sndBgRemove.onclick = () => {
-    takeAmount()
-    sndBgRemove.classList.remove('bg-Lightgrayishblue1')
+        takeAmount()
+        sndBgRemove.classList.remove('bg-Lightgrayishblue1')
+        sndBgRemove.onclick = null
 }
 
 thirdBgRemove.onclick = () => {
-    takeAmount()
-    thirdBgRemove.classList.remove('bg-Lightgrayishblue1')
+        takeAmount()
+         thirdBgRemove.classList.remove('bg-Lightgrayishblue1')
+         thirdBgRemove.onclick = null
 }
